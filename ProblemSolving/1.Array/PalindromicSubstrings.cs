9181035f432db1e25771d7213b941e77432945bc
abhi <axm3737@g.rit.edu>
2022-05-21 22:05:02 -0400

    structure created

---
+++ ProblemSolving/1.Array/PalindromicSubstrings.cs
@@ -0,0 +1,14 @@
+public class PalindromicSubstrings 
+{
+    public int CountSubstrings(string s) 
+    {
+        return 0;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Main Method");
+        PalindromicSubstrings ps = new PalindromicSubstrings();
+        Console.WriteLine(ps.CountSubstrings("abhi"));
+    }
+}
